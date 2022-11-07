4881399d90e7200f31e120828e6a2357d810499d
jjmontoy7 <103865389+jjmontoy7@users.noreply.github.com>
2022-11-07 01:50:20 -0500

    Create File.h

---
+++ File.h
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<fstream>
+#include<string.h>
+#include<stdlib.h>
+#include <algorithm>
+#include "Tree.h"
+
+using namespace std;
+
+class File{
+
+    public:
+        File();
+        void lectura();
+        void split(Nodo*, string);
+        //void split(string);
+
+};
