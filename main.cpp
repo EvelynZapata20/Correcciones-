dd2200c36c4e9af7a3bf5bf3fe0875791283772f
jjmontoy7 <103865389+jjmontoy7@users.noreply.github.com>
2022-11-07 15:18:59 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,19 +2,16 @@
 #include<conio.h>
 #include<stdlib.h>
 #include "File.h"
-//#include "Tree.h"
 
 using namespace std;
 
 void Menu();
-//Nodo *arbol = new Nodo("Hormiga","7");
 Nodo *arbol;
 File *archivo = new File();
 
 int main(){
 
-	Menu();
-	
+	Menu();	
 	getch();
 	return 0;
 }
@@ -26,37 +23,37 @@ void Menu(){
 	string dato;
 	
 	do{
+		cout<<"\n==== Menu Principal ====\n";
 		cout<<"1. Agregar elementos al arbol. "<<endl;
 		cout<<"2. Imprimir Arbol"<<endl;
-		cout<<"3. Buscar Dato"<<endl;
-        cout<<"4. Salir."<<endl;
-		cout<<"\nOpcion: ";
+		cout<<"3. Buscar Termino"<<endl;
+		cout<<"4. Buscar Subtermino"<<endl;
+        cout<<"5. Salir.\n"<<endl;
+		cout<<"Opcion: ";		
 		cin>>opcion;
 		
+		
 		switch(opcion){
-			case 1:	archivo->lectura();/*					
-                    arbol->agregarNodo(arbol,"Hormiga","7");
-                    arbol->agregarNodo(arbol,"Gato","4");
-                    arbol->agregarNodo(arbol,"Perro","5");
-                    arbol->agregarNodo(arbol,"Oveja","3");
-                    arbol->agregarNodo(arbol,"Vaca","8");
-                    arbol->agregarNodo(arbol,"Abeja","6");
-                    arbol->agregarNodo(arbol,"Buho","2");
-                    arbol->agregarNodo(arbol,"Tigre","1");
-                    //*/
+			case 1:	arbol =	archivo->lectura();
 					break;
 			case 2: cout<<"---- Imprimiendo Arbol ----\n\n"<<endl;
                     arbol->imprimir(arbol);
                     cout<<"\n";
                     system("pause");
                     break;
-			case 3: cout<<"Ingrese valor a buscar:"<<endl;
+			case 3: cout<<"Ingrese termino a buscar:"<<endl;
 					cin>>dato;
 					arbol->buscar(arbol, dato);					
 					break;
-        break;
+			case 4: cout<<"Ingrese subtermino a buscar:"<<endl;
+					cin>>dato;
+					arbol->buscarSub(arbol, "\t"+dato);					
+					break;
+			case 5: cout<<"Cerrando programa!\n";
+					break;
+			break;
 		}
 		system("pause");
 		system("cls");
-	}while(opcion != 4);
+	}while(opcion != 5);
 }
